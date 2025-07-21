4fb0584f9e4dfa62b08779cda90305598dedc5d3
prathameshNDelxn <prathameshnarawade.delxn@gmail.com>
2025-07-21 12:37:23 +0530

    Auth

--- registerAdmin.js
+++ registerAdmin.js
@@ -1,7 +1,8 @@
 const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 
 async function registerAdmin() {
-  const response = await fetch('http://localhost:9000/api/v1/auth/register/admin', {
+  const API_BASE_URL = process.env.BACKEND_API_BASE_URL || 'http://localhost:9000';
+  const response = await fetch(`${API_BASE_URL}/api/v1/auth/register/admin`, {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({
