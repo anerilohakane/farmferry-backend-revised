5868029d4fa2effeb9323ccf47b2834fab69edf8
Kanika Bhawsar <bhawsarkanika.delxn@gmail.com>
2025-07-28 14:37:24 +0530

    added damaged status

--- models/category.model.js
+++ models/category.model.js
@@ -10,7 +10,7 @@ const categorySchema = new mongoose.Schema(
     },
     subCategory:{
       type: String,
-      required:false
+      required:true
     },
     description: {
       type: String,
