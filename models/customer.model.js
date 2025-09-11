56980eef43322cf663e02652541f4ca122297c04
anerilohakane <aneridelxn@gmail.com>
2025-09-11 18:11:52 +0530

    db change

--- models/customer.model.js
+++ models/customer.model.js
@@ -65,7 +65,8 @@ const customerSchema = new mongoose.Schema({
   email: { 
     type: String, 
     //required: [true, "Email is required"], 
-    unique: true, 
+    //unique: true, 
+    sparse: true,
     lowercase: true, 
     trim: true,
     match: [/^\S+@\S+\.\S+$/, 'Please enter a valid email address']
