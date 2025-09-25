5ec90c2a9b38139a1950f61f7c11af1ec93d7c01
anerilohakane <aneridelxn@gmail.com>
2025-09-25 18:17:28 +0530

    CORS

--- app.js
+++ app.js
@@ -50,26 +50,19 @@ const allowedOrigins = [
   'https://farmferry.in'
 ];
 
-// const corsOptions = {
-//   origin: (origin, callback) => {
-//     if (!origin) return callback(null, true); // allow non-browser clients
-//     const normalized = origin.replace(/\/$/, '');
-//     if (allowedOrigins.includes(normalized)) {
-//       return callback(null, true);
-//     }
-//     return callback(null, false);
-//   },
-//   credentials: true,
-//   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
-//   allowedHeaders: ['Content-Type', 'Authorization']
-// };
-
 const corsOptions = {
-  origin: "*",
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
-  allowedHeaders: ["Content-Type", "Authorization"],
+  origin: (origin, callback) => {
+    if (!origin) return callback(null, true); // allow non-browser clients
+    const normalized = origin.replace(/\/$/, '');
+    if (allowedOrigins.includes(normalized)) {
+      return callback(null, true);
+    }
+    return callback(null, false);
+  },
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
+  allowedHeaders: ['Content-Type', 'Authorization']
 };
-
 
 app.use(cors(corsOptions));
 app.options('*', cors(corsOptions));
