8a7c1d48511ce60421015119ae7ee2a0b025d28f
anerilohakane <aneridelxn@gmail.com>
2025-12-29 13:02:56 +0530

    Twilio

--- sendSMStest.js
+++ sendSMStest.js
@@ -3,7 +3,7 @@ import twilio from "twilio";
 const accountSid = "AC185367e43d7ec6d32b8b7aa7ba16ee5e";
 const authToken = "bc0d1b1632534afdba15a721f1e1bd6a";
 const fromNumber = "+12272309426"; // e.g. +16824705397
-const toNumber = "+919322506730"; // your phone number in E.164 format (+91XXXXXXXXXX)
+const toNumber = "+919322506730"; 
 
 const client = twilio(accountSid, authToken);
 
